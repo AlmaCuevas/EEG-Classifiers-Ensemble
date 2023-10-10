1cbd3e8b34ac5433fb94f7efde39495d32c9441a
Alma Rosa Cuevas Romero <almacuevas@192.168.1.9>
2023-10-10 10:03:02 -0600

    Add basic info and start the README.md

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# EEG Data Classification with CNN, LSTM/GRU, and Mixed LSTM Models
+# Voting system EEG Data Classification
 
-description of datasets: http://www.bbci.de/competition/iv/desc_2a.pdf
+All the codes here are from other contributors:
+* LMDA-Net
+* BigProject
+* CCSPNet
+They were modified to accomodate the needs for the project.
+
+#  The Datasets are provided by:
+* Aguilera
+* Nieto
+* Coretto
+* Torres
+
+#TODO:
+* Install the minimum amount of dependencies to make sure those are the ones that the project really needs. You need to go to requirements.txt and check none are missing or are extra.
+* Cite code
+* Cite datasets and mention why they were chosen
--- share.py
+++ share.py
@@ -2,36 +2,37 @@
 # DATASETS BASIC INFO
 
 # Edgar Aguilera Tradicional
-aguilera_info = {'mi_class': 4, # avanzar, retroceder, derecha and izquierda
-'channels': 24,
+aguilera_info = {'#_class': 4, # avanzar, retroceder, derecha and izquierda
+'#_channels': 24,
 'samples':0, # PENDING
 'sample_rate': 500,
 'channels_names': [], # PENDING
 'subjects':15}
 
 # Nieto
-nieto_info = {'mi_class': 4, # arriba, abajo, derecha, izquierda
-'channels': 128,
+nieto_info = {'#_class': 4, # arriba, abajo, derecha, izquierda
+'#_channels': 128,
 'samples': 0, # PENDING
-'sample_rate':1024, # or 254 pending check
+'sample_rate': 256, # in BDF: 1024, but 256 is what the Python extraction tutorial they provided says.
 'channels_names': [], # PENDING
 'subjects':10}
 
 # Coretto
 # La frecuencia de muestreo se estrablecio en 1024Hz. De modo que cada intervalo de habla imaginada consta de 4096 muestras (4 segundos). Se implementaron filtrado digital pasabanda con frecuencias de paso de 2 y 45 HZ.
-coretto_info = {'mi_class': 6, # arriba, abajo, izquierda, derecha, adelante and atrás (plus vocals: a, e, i, o, u)
-'channels': 6,
+# De modo que el registro de una palabra esta constituido por 24576 muestras correspondientes a los canales de EEG, más tres muestras adicionales que indican la modalidad, estimulo y la presencia de artefactos oculares.
+coretto_info = {'#_class': 6, # arriba, abajo, izquierda, derecha, adelante and atrás (plus vocals: a, e, i, o, u)
+'#_channels': 6,
 'samples': 0, # PENDING
 'sample_rate': 1024,
 'channels_names': ['F3', 'F4', 'C3', 'C4', 'P3', 'P4'],
 'subjects':15}
 
 # Torres
-torres_info = {'mi_class': 5, # arriba, abajo, izquierda, derecha and seleccionar
-'channels': 14, # CHECK
+torres_info = {'#_class': 5, # arriba, abajo, izquierda, derecha and seleccionar
+'#_channels': 14, # CHECK
 'samples': 0, # PENDING
 'sample_rate': 128,
 'channels_names': [], # PENDING
 'subjects':27}
 
-pipeline = [aguilera_info, nieto_info, coretto_info, torres_info] # If one day I decide to run all experiments at once
+datasets_basic_infos = {'aguilera':aguilera_info, 'nieto':nieto_info, 'coretto':coretto_info, 'torres':torres_info} # If one day I decide to run all experiments at once
