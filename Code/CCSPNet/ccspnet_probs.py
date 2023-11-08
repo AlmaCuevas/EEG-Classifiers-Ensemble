c3bfe86e1ed1db530b2ed27576acee88d0c52ce3
Alma Rosa Cuevas Romero <almacuevas@MacBook-Pro-de-Alma-2.local>
2023-11-08 16:29:28 -0600

    ccspnet prediction

--- Code/CCSPNet/ccspnet_probs.py
+++ Code/CCSPNet/ccspnet_probs.py
@@ -1,20 +1,83 @@
+from ccspnet import manual_seed, EEG_dataset, MVLoss, CCSP, train, evaluate, predict_answer, EEG_dataset_online
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+
 from share import datasets_basic_infos
 from data_loaders import load_data_labels_based_on_dataset
 import time
 
 #TODO: Load the CCSPNET file and divide it in train and test, find a way to provide the array from pairs.
 # project_combine_cnn_lstm_run_3.ipynb does something similar, it didn't gave good result but you could try.
+# We set the seed to 2045 because the Singularity is near!
+# manual_seed(2045)
 
-def ccspnet_train(data, y):
+if (torch.cuda.is_available()):
+    device = 'cuda'
+    workers = 2
+else:
+    device = 'cpu'
+    workers = 0
+print(f'Device is set to {device}\nNumber of workers: {workers}')
 
-    return clf
+def ccspnet_train(dataset, dataloader):
+    """#Run Network
 
-def ccspnet_test(clf, epoch):
+    Mode parameter: 'SD' for subject-dependent
+    """
+
+    hists_acc = []
+    hists_loss = []
+    manual_seed(2045)
+    net_args = {"kernLength": 32,
+                "timepoints": 250,
+                "wavelet_filters": 4,
+                "wavelet_kernel": 64,
+                "nn_layers": [4],
+                "feature_reduction_network": [16, 8, 4],
+                "n_CSP_subspace_vectors": 2,
+                "nb_classes": dataset_info['#_class'],
+                "nchans": dataset_info['#_channels'],
+                }
+
+    train_args = {"dataloader": dataloader,
+                  "epochs": 20,
+                  "lr": 0.01,
+                  "wavelet_lr": 0.001,
+                  "loss_ratio": 0.3,
+                  "criterion": MVLoss,
+                  "verbose": 0,
+                  "tensorboard": False,
+                  "m": net_args['n_CSP_subspace_vectors'],
+                  'lambda1': 0.01,
+                  'lambda2': 0.1
+                  }
+
+    net = CCSP(**net_args).to(device)
+
+    history = train(dataset=dataset, model=net, **train_args)
+    ### Evaluate
+    best_model_state = history['model'][np.argmin(history['train_loss'])]
+    print('The Best Epoch:', np.argmin(history['train_loss']) + 1)
+    test_loss, ts_acc = evaluate(net, best_model_state, dataloader, m=net_args['n_CSP_subspace_vectors'],
+                                 criterion=MVLoss,
+                                 loss_ratio=train_args["loss_ratio"], verbose=1, )
+    hists_acc.append([test_loss, ts_acc])
+
+    best_model_state = history['model'][np.argmax(history['train_accuracy'])]
+    print('The Best Epoch based on accuracy:', np.argmax(history['train_accuracy']) + 1)
+    test_loss, ts_acc = evaluate(net, best_model_state, dataloader, m=net_args['n_CSP_subspace_vectors'],
+                                 criterion=MVLoss,
+                                 loss_ratio=train_args["loss_ratio"], verbose=1, )
+    hists_loss.append([test_loss, ts_acc])
+    return net, best_model_state
+
+def ccspnet_test(model, epoch, best_model_state):
     """
     This is what the real-time BCI will call.
     Parameters
     ----------
-    clf : classifier trained for the specific subject
+    model : classifier trained for the specific subject
     epoch: one epoch, the current one that represents the intention of movement of the user.
 
     Returns Array of classification with 4 floats representing the target classification
@@ -25,8 +88,13 @@ def ccspnet_test(clf, epoch):
     #loaded_model = pickle.load(open(filename, 'rb'))
 
     # To see the array of predictions
-    array = clf.predict_proba(epoch)
-    return array
+    dataloader =EEG_dataset_online(epoch)
+    dataloader = DataLoader(dataset, batch_size=5300,
+                            num_workers=workers, pin_memory=True,
+                            shuffle=True)
+    answer = predict_answer(model, best_model_state, dataloader)
+
+    return answer # This is not an array. It's a 1 or 0
 
 if __name__ == '__main__':
     # Manual Inputs
@@ -41,21 +109,27 @@ if __name__ == '__main__':
     data_path = computer_root_path + dataset_foldername
     dataset_info = datasets_basic_infos[dataset_name]
 
-    data, y = load_data_labels_based_on_dataset(dataset_name, subject_id, data_path, array_format=array_format)
+    data, label = load_data_labels_based_on_dataset(dataset_name, subject_id, data_path, array_format=array_format)
     target_names = dataset_info['target_names']
+
+    dataset = EEG_dataset(data, label, 1) # true_label=1, so 1 wil be 1 and everyone else 0
+    ### Define Dataloader
+    dataloader = DataLoader(dataset, batch_size=5300,
+                            num_workers=workers, pin_memory=True,
+                            shuffle=True)
 
     print("******************************** Training ********************************")
     start = time.time()
-    clf = ccspnet_train(data, y)
+    model, best_model_state = ccspnet_train(dataset, dataloader)
     end = time.time()
     print("Training time: ", end - start)
 
     print("******************************** Test ********************************")
     epoch_number = 0
     start = time.time()
-    xdawn_array = ccspnet_test(clf, data[epoch_number])
+    answer = ccspnet_test(model, data[epoch_number], best_model_state)
     end = time.time()
     print("One epoch, testing time: ", end - start)
     print(target_names)
-    print("Probability: " , xdawn_array)
-    print("Real: ", y[epoch_number])
+    print("Probability: " , answer)
+    print("Real: ", label[epoch_number])
